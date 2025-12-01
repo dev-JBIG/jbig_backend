56039fba78128a2e0466aa5d12abdac3093eb162
sisoin <sokk159dps@gmail.com>
2025-12-01 22:50:26 +0900

    인증번호 고침

--- jbig_backend/settings.py
+++ jbig_backend/settings.py
@@ -101,8 +101,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'jbig_backend.wsgi.application'
 
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
